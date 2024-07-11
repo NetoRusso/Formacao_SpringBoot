5ca2eed070425ea048156136fd17e2fbf55235c3
Milena <milenaleme4@hotmail.com>
2024-07-11 19:18:04 -0300

    colocando meu nome

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -27,5 +27,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
 
+		System.out.println("Aqui é a Milena");
 	}
 }
